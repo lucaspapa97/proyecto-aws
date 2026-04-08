4885d98782dbb9091228d47da3b640153a796f58
lucaspapa97 <lucasivanpapa@gmail.com>
2026-04-08 12:28:01 -0300

    Add AWS Lambda function to return a greeting message

--- lambda/test1/lambda_function.py
+++ lambda/test1/lambda_function.py
@@ -1,1 +1,13 @@
+import json
 
+def lambda_handler(event, context):
+    # Mensaje de respuesta
+    mensaje = "Hola Mundo desde AWS Lambda!"
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': mensaje,
+            'input_event': event
+        })
+    }
